d9a780e94609863333ce01422e9342058ef952de
guilherme-hl1ma <guihlima@outlook.com>
2025-12-27 11:58:20 -0300

    feat: add basic auth middleware

--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
 from typing import Union
+from dotenv import load_dotenv
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlmodel import select
+from src.database import SessionDep
+from src.middleware import BasicAuthMiddleware
+from src.models import User
+
+load_dotenv()
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+app.add_middleware(BasicAuthMiddleware)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class UserAuth(BaseModel):
+    email: str
+    password: str
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/signup")
+def signup(user: User, session: SessionDep):
+    session.add(user)
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/users")
+async def get_users(session: SessionDep):
+    return session.exec(select(User)).all()
--- src/middleware.py
+++ src/middleware.py
@@ -1,12 +1,39 @@
-from pydantic import BaseModel
-from main import app
+import base64
+from fastapi import Response
+from sqlmodel import Session, select
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from src.models import User
+from src.database import engine
 
 
-class BasicAuthHeader(BaseModel):
-    email: str
-    password: str
+class BasicAuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        whitelist_paths = ["/signup", "/docs", "/openapi.json", "/redoc"]
+        path = request.url.path
+        if path in whitelist_paths:
+            return await call_next(request)
 
+        auth = request.headers.get("Authorization")
 
-@app.middleware("http")
-async def basic_auth():
-    pass
+        if not auth:
+            return Response(
+                status_code=401,
+                headers={"WWW-Authenticate": 'Basic realm="Login Required"'},
+            )
+
+        _, b64_credentials = auth.split(" ", 1)
+        byte_credentials = base64.b64decode(b64_credentials)
+        decoded_credentials = byte_credentials.decode("utf-8")
+        email, password = decoded_credentials.split(":")
+
+        with Session(engine) as session:
+            user = session.exec(
+                select(User).where(User.email == email, User.password == password)
+            ).first()
+            if not user:
+                return Response(
+                    status_code=401,
+                    headers={"WWW-Authenticate": 'Basic realm="Login Required"'},
+                )
+        return await call_next(request)
